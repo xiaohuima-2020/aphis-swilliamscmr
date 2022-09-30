f6eef877f316bfd3c8b3cedbb30608c96533edb1
Allison Carnwath <acarnwath@reingold.com>
2022-09-30 18:09:24 -0400

    AWM-399 Overriding USWDS with APHIS button styles. Some quick tweaks to spacing on the page intro component now that the correct button is slotted in.

--- web/themes/aphis/source/02-uswds/button/button.stories.jsx
+++ web/themes/aphis/source/02-uswds/button/button.stories.jsx
@@ -4,6 +4,7 @@ import twigTemplate from './button.twig';
 import data from './button.yml';
 import secondaryData from './button--secondary.yml';
 import unstyledData from './button--unstyled.yml';
+import textData from './button--text.yml';
 import '../uswds.es6';
 
 const settings = {
@@ -48,5 +49,17 @@ Unstyled.parameters = {
   },
 };
 
+const Text = args => (
+  parse(twigTemplate({
+    ...args,
+  }))
+);
+Text.args = { ...textData };
+Text.parameters = {
+  docs: {
+    storyDescription: "Custom variation, extended from https://designsystem.digital.gov/components/button/.",
+  },
+};
+
 export default settings;
-export { Default, Secondary, Unstyled };
+export { Default, Secondary, Unstyled, Text };
