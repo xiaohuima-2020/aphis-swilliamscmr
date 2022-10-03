fa1921e6c57603311194f9a5450ac13f2dc9d4a7
Shawn Williams <swilliams@cmrignite.com>
2022-10-03 13:52:15 -0400

    more clean up of custom functions module with phpbcf

--- web/modules/custom/aphis_custom_functions/src/Controller/AphisCustomFunctionsController.php
+++ web/modules/custom/aphis_custom_functions/src/Controller/AphisCustomFunctionsController.php
@@ -1,34 +1,25 @@
 <?php
 
-/**
- * Aphis Custom Functions
- *
- * @category AphisCustomFunctions
- * @package  Aphis
- * @author   Shawn Williams
- * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
- * @link     http://www.cmrignite.com/
- */
-
 namespace Drupal\aphis_custom_functions\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 
 /**
- * Aphis Custom Functions
+ * Aphis Custom Functions.
  *
  * @category Class
- * @package  Aphis
- * @author   Shawn Williams
- * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
- * @link     http://www.cmrignite.com/
+ * @package Aphis
+ * @author Shawn Williams
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link http://www.cmrignite.com/
  */
-class AphisCustomFunctionsController
-{
+class aphisCustomFunctionsController {
 
-    public function aphis_custom_functions()
-    {
-        return new Response('Hello to Aphis Custom Functions');
-    }
+  /**
+   * Returns a response.
+   */
+  public function aphis_custom_functions() {
+    return new Response('Hello to Aphis Custom Functions');
+  }
 
 }
